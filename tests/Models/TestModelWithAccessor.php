669fba8249249c409cd4eb8743b386a80c3b1cbf
Ali <ali@brainlet.co>
2021-01-24 14:59:40 +0500

    Improve coverage by testing the accessor method available on the table.

---
+++ tests/Models/TestModelWithAccessor.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Brainlet\LaravelConvertTimezone\Tests\Models;
+
+use Brainlet\LaravelConvertTimezone\Traits\ConvertTZ;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class TestModelWithAccessor extends \Illuminate\Database\Eloquent\Model
+{
+    protected $table = 'models';
+
+    use HasFactory, ConvertTZ;
+
+    protected $guarded = [];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return $value;
+    }
+}
