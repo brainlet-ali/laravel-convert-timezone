c77dcac847878ab6321ebf2dcc68d3d1921bca86
Ali Raza <brainlet.ali@gmail.com>
2021-01-24 14:33:52 +0500

    Change table name to more generalized name models.

--- tests/Models/TestModel.php
+++ tests/Models/TestModel.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TestModel extends \Illuminate\Database\Eloquent\Model
 {
+    protected $table = 'models';
+
     use HasFactory, ConvertTZ;
 
     protected $guarded = [];
