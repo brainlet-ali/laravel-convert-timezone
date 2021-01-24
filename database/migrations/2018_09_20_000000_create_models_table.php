1fa76578fb4d5b425d7ba50ba03283cc9ae35f74
Ali <ali@brainlet.co>
2021-01-24 14:33:52 +0500

    Change table name to more generalized name models.

--- database/migrations/2018_09_20_000000_create_models_table.php
+++ database/migrations/2018_09_20_000000_create_models_table.php
@@ -4,11 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMyModelsTable extends Migration
+class CreateModelsTable extends Migration
 {
     public function up()
     {
-        Schema::create('my_models', function (Blueprint $table) {
+        Schema::create('models', function (Blueprint $table) {
             $table->bigIncrements('id');
 
             $table->timestamps();
