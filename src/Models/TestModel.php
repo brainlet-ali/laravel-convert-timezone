db784f4e3c3da9975262c387000e1d987d6474d9
Ali Raza <brainlet.ali@gmail.com>
2023-01-22 12:07:33 +0500

    Move model classes out of test directory

--- src/Models/TestModel.php
+++ src/Models/TestModel.php
@@ -1,20 +1,15 @@
 <?php
 
-namespace Brainlet\LaravelConvertTimezone\Tests\Models;
+namespace Brainlet\LaravelConvertTimezone\Models;
 
 use Brainlet\LaravelConvertTimezone\Traits\ConvertTZ;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class TestModelWithAccessor extends \Illuminate\Database\Eloquent\Model
+class TestModel extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'models';
 
     use HasFactory, ConvertTZ;
 
     protected $guarded = [];
-
-    public function getCreatedAtAttribute($value)
-    {
-        return $value;
-    }
 }
