7eed7de9ca56aceb36f5a475e6b5936c846abdcc
Ali Raza <brainlet.ali@gmail.com>
2023-01-22 12:16:41 +0500

    Fix styling

--- src/Exceptions/InvalidTimezone.php
+++ src/Exceptions/InvalidTimezone.php
@@ -6,7 +6,6 @@ use InvalidArgumentException;
 
 class InvalidTimezone extends InvalidArgumentException
 {
-
     public static function make(string $timezone): self
     {
         return new static("The timezone `{$timezone}` is invalid.");
