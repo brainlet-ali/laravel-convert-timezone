6234598d62ac65c48195ac3a79c814a80c279dab
Ali Raza <brainlet.ali@gmail.com>
2023-01-22 13:56:23 +0500

    Retire Lumen checks

--- src/LaravelConvertTimezoneServiceProvider.php
+++ src/LaravelConvertTimezoneServiceProvider.php
@@ -23,11 +23,6 @@ class LaravelConvertTimezoneServiceProvider extends ServiceProvider
 
     protected function offerPublishing()
     {
-        if (! function_exists('config_path')) {
-            // function not available and 'publish' not relevant in Lumen
-            return;
-        }
-
         $this->publishes([
             __DIR__.'/../config/tz.php' => config_path('tz.php'),
         ], 'tz-config');
