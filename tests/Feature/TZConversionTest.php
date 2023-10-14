78fc786e04438e70f11bbbd560aeb6e470023031
Ali <ali@brainlet.co>
2023-10-14 10:09:56 +0500

    Update timezone conversion tests to improve database interaction
    Revised test cases in TZConversionTest.php to minimize database interaction. Instead of using a created model directly, a new instance is fetched from the database before asserting time conversions. This adjustment improves test reliability when dealing with database time conversions and ensures that the time stored in the database is the time being tested.

--- tests/Feature/TZConversionTest.php
+++ tests/Feature/TZConversionTest.php
@@ -8,12 +8,14 @@ use Carbon\Carbon;
 it('converts utc time to destination timezone', function () {
     $utcTime = now(); // UTC
 
-    $model = TestModel::factory()->create(['created_at' => $utcTime]);
+    TestModel::factory()->create(['created_at' => $utcTime]);
 
     config(['tz.timezone' => 'Asia/Karachi']);
 
     $destinationTime = Carbon::parse($utcTime)
         ->addHours(5)->format('Y-m-d H:i:s'); // UTC (+5) [Asia/Karachi]
+
+    $model = TestModel::first();
 
     $convertedTime = $model->created_at;
 
@@ -25,27 +27,28 @@ it('converts utc time to destination timezone', function () {
 it('never converts date/time filed if accessor method is available', function () {
     $utcTime = now(); // UTC
 
-    $model = TestModelWithAccessor::factory()->create(['created_at' => $utcTime]);
+    TestModelWithAccessor::factory()->create(['created_at' => $utcTime]);
 
     config(['tz.timezone' => 'Asia/Karachi']);
 
     $destinationTime = Carbon::parse($utcTime)
         ->addHours(5)->format('Y-m-d H:i:s'); // UTC (+5) [Asia/Karachi]
 
+    $model = TestModelWithAccessor::first();
+
     $convertedTime = $model->created_at;
 
-    expect($convertedTime)
-        ->not->toBeInstanceOf(Carbon::class)
-        ->and($convertedTime)->not->toBe($destinationTime);
+    expect($convertedTime)->not->toBe($destinationTime);
 });
 
 it('throws exception if timezone is invalid', function () {
     config(['tz.timezone' => 'invalid-timezone']);
 
-    $model = TestModel::factory()->create(['created_at' => now()]);
+    TestModel::factory()->create(['created_at' => now()]);
+
+    $model = TestModel::first();
 
     expect($model->created_at)
         ->not->toBeInstanceOf(Carbon::class);
-})->
-throws(InvalidTimezone::class,
+})->throws(InvalidTimezone::class,
     'The timezone `invalid-timezone` is invalid.');
